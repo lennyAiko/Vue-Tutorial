23567e431a345e140e94ad27a6d968deec8994e9
Lennox Charles <lennyaiko17@gmail.com>
2023-07-31 19:43:06 +0100

    feat: router works

--- PROJECT 4/RouterVueLearning/src/App.vue
+++ PROJECT 4/RouterVueLearning/src/App.vue
@@ -1,9 +1,16 @@
 <script setup>
+  import {ref} from "vue"
   import {RouterView} from "vue-router"
+  import Nav from "./components/Nav.vue"
+
+  const randomState = ref("")
 </script>
 
 <template>
   <div>
+    {{ randomState }}
+    <button @click="randomState = 'hello there'">Click me</button>
+    <Nav /> 
     <RouterView />
   </div>
 </template>
