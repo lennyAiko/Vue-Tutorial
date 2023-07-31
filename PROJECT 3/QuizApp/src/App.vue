899fa287d3e80838c238881be76bf694c8fba706
Lennox Charles <lennyaiko17@gmail.com>
2023-07-31 17:09:25 +0100

    feat: progress on project 3

---
+++ PROJECT 3/QuizApp/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+
+  import q from './data/quizes.json'
+  import { ref, watch } from 'vue'
+  import Card from './components/Card.vue';
+
+  const quizes = ref(q)
+  const search = ref('')
+
+  watch(search, () => {
+    quizes.value = q.filter(quiz => quiz.name.toLowerCase().includes(search.value.toLowerCase()))
+  })
+
+</script>
+
+<template>
+  <div class="mx-auto my-0 max-w-5xl">
+    <header class="mb-2.5 mt-7 flex items-center">
+      <h1 class="font-bold mr-7">Quizes</h1>
+      <input v-model.trim="search" type="text" placeholder="Search..."
+      class="border-none bg-slate-700 opacity-10 text-white p-2.5 rounded-md">
+    </header>
+
+    <div class="flex flex-wrap mt-10">
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" /> 
+    </div>
+  </div>
+</template>
