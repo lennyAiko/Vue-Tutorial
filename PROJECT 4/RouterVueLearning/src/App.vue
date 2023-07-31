27eed8e63b2fc4fbe3038cbea3d07a785a3dc243
Lennox Charles <lennyaiko17@gmail.com>
2023-07-31 18:15:56 +0100

    chore: install vue-router

---
+++ PROJECT 4/RouterVueLearning/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import HomeView from './views/HomeView.vue'
+  import AboutView from './views/AboutView.vue'
+</script>
+
+<template>
+  <div>
+    <h1>Home View</h1>
+  </div>
+</template>
---
+++ PROJECT 4/RouterVueLearning/src/views/AboutView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="">
+        <h1 class="">About View</h1>
+    </div>
+</template>
