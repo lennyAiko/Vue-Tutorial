68c43864edcde1ed52f369c3224836e696d0003e
lennox charles <lennyaiko17@gmail.com>
2023-09-21 16:53:52 +0100

    feat: question templates is set

--- PROJECT 3/QuizApp/src/views/QuizView.vue
+++ PROJECT 3/QuizApp/src/views/QuizView.vue
@@ -6,15 +6,15 @@
                 <div class="h-full w-0 bg-yellow-50"></div>
             </div>
         </header>
-        <div class="">
-            <div class="">
-                <h1 class="">What is the chemical value?</h1>
+        <div>
+            <div class="mt-5"> <!-- question-container -->
+                <h1 class="text-4xl mb-5">What is the chemical value?</h1> <!-- question -->
             </div>
-            <div class="">
-                <div class="">
-                    <p class="">A</p>
-                    <div class="">
-                        <p>NACl</p>
+            <div class=""> <!-- options-container -->
+                <div class="flex mb-5 cursor-pointer"> <!-- option -->
+                    <p class="bg-yellow-50 w-[50px] h-[50px] text-3xl flex content-center justify-center">A</p> <!-- option-label -->
+                    <div class="bg-[rgb(244,239,239)] w-full text-3xl px-[20px] py-0"> <!-- option-value -->
+                        <p>NaCl</p>
                     </div>
                 </div>
             </div>
