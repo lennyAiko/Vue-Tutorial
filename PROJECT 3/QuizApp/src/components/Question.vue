17ead5600a799b26c3bf16ae6391063eac6ca4d8
lennox charles <lennyaiko17@gmail.com>
2023-09-22 00:17:56 +0100

    feat: get the proper questions

--- PROJECT 3/QuizApp/src/components/Question.vue
+++ PROJECT 3/QuizApp/src/components/Question.vue
@@ -1,12 +1,19 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    const {question} = defineProps(['question'])
+</script>
+
 <template>
     <div class="mt-5"> <!-- question-container -->
-        <h1 class="text-4xl mb-5">What is the chemical value?</h1> <!-- question -->
+        <h1 class="text-4xl mb-5">{{ question.text }}</h1> <!-- question -->
     </div>
     <div class=""> <!-- options-container -->
-        <div class="flex mb-5 cursor-pointer"> <!-- option -->
-            <p class="bg-yellow-50 w-[50px] h-[50px] text-3xl flex content-center justify-center">A</p> <!-- option-label -->
+        <div class="flex mb-5 cursor-pointer" 
+        v-for="option in question.options" :key="option.id"> <!-- option -->
+            <p class="bg-yellow-50 w-[50px] h-[50px] text-3xl flex content-center justify-center">{{ option.label }}</p> <!-- option-label -->
             <div class="bg-[rgb(244,239,239)] w-full text-3xl px-[20px] py-0"> <!-- option-value -->
-                <p>NaCl</p>
+                <p>{{ option.text }}</p>
             </div>
         </div>
     </div>
--- PROJECT 3/QuizApp/src/views/QuizView.vue
+++ PROJECT 3/QuizApp/src/views/QuizView.vue
@@ -1,13 +1,24 @@
 <script setup>
-import Question from '../components/Question.vue'
-import QuizHeader from '../components/QuizHeader.vue'
+    import { ref } from 'vue';
+    import Question from '../components/Question.vue'
+    import QuizHeader from '../components/QuizHeader.vue'
+    import {useRoute} from 'vue-router'
+    import quizes from '../data/quizes.json'
+
+    const route = useRoute()
+
+    const quizId = parseInt(route.params.id)
+
+    const quiz = quizes.find(q => q.id === quizId)
+
+    const currentQuestionIndex = ref(0)
 </script>
 
 <template>
     <div class="">
         <QuizHeader />
         <div>
-           <Question /> 
+           <Question :question="quiz.questions[currentQuestionIndex]" /> 
         </div>
     </div>
 </template>
