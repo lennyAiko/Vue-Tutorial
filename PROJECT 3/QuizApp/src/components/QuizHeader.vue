101c674b8208eae410cca4cf50b897aedee8cd1e
lennox charles <lennyaiko17@gmail.com>
2023-09-22 00:39:11 +0100

    feat: move to next question

--- PROJECT 3/QuizApp/src/components/QuizHeader.vue
+++ PROJECT 3/QuizApp/src/components/QuizHeader.vue
@@ -1,6 +1,14 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    const {questionStatus} = defineProps(['questionStatus'])
+
+    console.log(questionStatus)
+</script>
+
 <template>
     <header class="mt-5">
-        <h4 class="text-3xl">Question 1/3</h4>
+        <h4 class="text-3xl">Question {{ questionStatus }}</h4>
         <div class="w-72 h-12 border-4 border-solid border-yellow-50">
             <div class="h-full w-0 bg-yellow-50"></div>
         </div>
--- PROJECT 3/QuizApp/src/views/QuizView.vue
+++ PROJECT 3/QuizApp/src/views/QuizView.vue
@@ -1,5 +1,5 @@
 <script setup>
-    import { ref } from 'vue';
+    import { ref, watch } from 'vue';
     import Question from '../components/Question.vue'
     import QuizHeader from '../components/QuizHeader.vue'
     import {useRoute} from 'vue-router'
@@ -8,18 +8,23 @@
     const route = useRoute()
 
     const quizId = parseInt(route.params.id)
-
     const quiz = quizes.find(q => q.id === quizId)
-
     const currentQuestionIndex = ref(0)
+    const questionStatus = ref(`${currentQuestionIndex.value}/${quiz.questions.length}`)
+
+    watch(() => currentQuestionIndex.value, () => {
+        questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`
+    })
 </script>
 
 <template>
     <div class="">
-        <QuizHeader />
+        <QuizHeader :questionStatus="questionStatus"/>
         <div>
            <Question :question="quiz.questions[currentQuestionIndex]" /> 
         </div>
+
+        <button @click="currentQuestionIndex++">Next Question</button>
     </div>
 </template>
 
